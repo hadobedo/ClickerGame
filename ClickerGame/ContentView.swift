88531b6a8742a7f335c9aed70caed1702abf4ea6
Nick B <nicholas.e.bonello@gmail.com>
2020-08-13 00:16:42 -0400

    Initial Commit

---
+++ ClickerGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClickerGame
+//
+//  Created by Nick Bonello on 2020-08-13.
+//  Copyright © 2020 Nick Bonello. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
